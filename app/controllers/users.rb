94bfb09b551ec58f8440d9fd056889e3815b8e88
Faithsend <matt@sandfalls.com>
2015-06-17 16:23:58 -0500

    form and post route

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,19 @@
 post "/users/new" do
 
+  if params[:password] != params[:password_2]
+    #throw an error with ajax or something if this gets passed
+    redirect to '/users/new'
+  else
+    user = User.new(params[:user])
+    user.password = params[:password]
+
+    if user.save
+      session[:user_id] = user.user_id
+      redirect to '/'
+    else
+      erb :"/users/new"
+    end
+  end
 end
 
 get '/users/new' do
