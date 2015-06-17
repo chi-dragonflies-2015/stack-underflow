31280d28add70be5ccb51bdf8de2f7004610a4e7
Matt & peterhinners <pair+faithsend.peterhinners@devbootcamp.com>
2015-06-17 14:45:37 -0500

    answers and comments seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,42 @@ require 'Faker'
                email: Faker::Internet.email)
 end
 
-
-
 10.times do
   id = 1 + rand(10)
   user = User.find(id)
-  Question.create!(
+  user.questions.create!(
+    title: Faker::Company.bs,
+    body: Faker::Lorem.paragraph
+    )
+
+  comment_count = 1 + rand(6)
+  comment_count.times do
+    user_id = 1 + rand(10)
+    commenter = User.find(user_id)
+    Question.last.comments.create!(body: Faker::Company.catch_phrase, user: commenter)
+  end
+end
+
+20.times do
+  id = 1 + rand(10)
+  user = User.find(id)
+  question = Question.find(id)
+  question.answers.create!(
     title: Faker::Company.bs,
     body: Faker::Lorem.paragraph,
     user: user
     )
 
+  comment_count = 1 + rand(6)
+  comment_count.times do
+    user_id = 1 + rand(10)
+    commenter = User.find(user_id)
+    Answer.last.comments.create!(body: Faker::Company.catch_phrase, user: commenter)
+  end
+
+
 end
+
+
+
+
